0e61f1fd7edc138fda9b806814da8a2cbe59c49e
Ardis84 <ardis84s@gmail.com>
2018-02-06 10:07:57 +0100

    modifiche

---
+++ BachecaFX/src/b/printfoot/AssegnaComitive.java
@@ -0,0 +1,50 @@
+package b.printfoot;
+
+import javafx.scene.control.ComboBox;
+
+public class AssegnaComitive {
+
+		String data, giorno, luogo, ora, id;
+		
+		ComboBox<String> conduttore;
+		
+		public String getData() {
+			return data;
+		}
+		public void setData(String data) {
+			this.data = data;
+		}
+		public String getGiorno() {
+			return giorno;
+		}
+		public void setGiorno(String giorno) {
+			this.giorno = giorno;
+		}
+		public String getLuogo() {
+			return luogo;
+		}
+		public void setLuogo(String luogo) {
+			this.luogo = luogo;
+		}
+		public String getOra() {
+			return ora;
+		}
+		public void setOra(String ora) {
+			this.ora = ora;
+		}
+		public ComboBox<String> getConduttore() {
+			return conduttore;
+		}
+		public void setConduttore(ComboBox<String> conduttore) {
+			this.conduttore = conduttore;
+		}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		
+		
+	
+}
